03f599026d02c1aafc85cc44ebc8be1cb2c6e8f5
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2024-06-16 00:04:57 -0300

    Update users controller
    Criação de classe UsersController
    create user: name, email, username, password

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,1 +1,31 @@
 import { Request, Response } from "express";
+import { prismaConnection } from "../database/prisma.connection";
+
+export class UsersController {
+	public static async create(req: Request, res: Response) {
+		try {
+			const { name, email, username, password } = req.body;
+
+			await prismaConnection.user.create({
+				data: {
+					name,
+					email,
+					username,
+					password,
+				},
+			});
+
+			return res.status(201).json({
+				ok: true,
+				message: "Usuario cadastrado com sucesso!",
+			});
+		} catch (err) {
+			return res.status(500).json({
+				ok: false,
+				message: `Ocorreu um erro inesperado. Erro: ${(err as Error).name} - ${
+					(err as Error).message
+				}`,
+			});
+		}
+	}
+}
